697853f314fa2225e39444e0a2e2451f39f5906c
reallybigsky <do.not.focus@gmail.com>
2024-03-14 22:58:28 +0300

    small typo fix

--- application/server/src/main.rs
+++ application/server/src/main.rs
@@ -7,13 +7,13 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 3 {
-        panic!("Must specify 3 arguments")
+        panic!("Must specify 2 arguments")
     }
     
     let port = args.get(1)
-        .expect("Must specify 3 arguments");
+        .expect("Must specify 2 arguments");
     let n_workers = args.get(2)
-        .expect("Must specify 3 arguments")
+        .expect("Must specify 2 arguments")
         .parse::<usize>()
         .expect("concurrency level must be a number");
     let server = HttpServer::bind(port, n_workers)
